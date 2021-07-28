c0d04a9ccfa638da6216fb9c873a6d5563323c7e
504 <504@DESKTOP-B0KVD9C>
2021-07-28 18:03:30 +0900

    일단 진행

--- two/src/chap15/six/Queue/QueueExample.java
+++ two/src/chap15/six/Queue/QueueExample.java
@@ -1,9 +1,23 @@
 package chap15.six.Queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+
+
 public class QueueExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Queue<Message>messagesQueue =new LinkedList<Message>();
+		
+		messagesQueue.offer(new Message("sendMail","홍길동"));
+		messagesQueue.offer(new Message("sendSMS","신용권"));
+		messagesQueue.offer(new Message("sendMail","홍두께"));
+		
+		while (!messagesQueue.isEmpty()) {
+			Message message = MessageQueue.poll()
+		}
 
 	}
 
